c5864ea7f194e898844395a48f6270dafa1f57d5
Fredrik Morten Sætran <fredrikm@mnemonic.no>
2024-09-23 12:39:07 +0200

    remove erroneous import

--- nvd_data_preprocess/main.py
+++ nvd_data_preprocess/main.py
@@ -10,12 +10,11 @@ from feature_extraction import process_cve_entries
 from cwe_cvss_prediction.cvss_classifier import train_cvss_model
 from cwe_cvss_prediction.cwe_classifier import train_cwe_classifier
 from predict_cvss_cwe_for_nvd import predict_cwe_and_cvss_for_nvd_data
-from create_training_set import feature_generation
 
 DATA_DIR = Path(__file__).parent.parent / "data"
 
 NVD_FILEPATH = DATA_DIR / "cve_corpus"
-DATA_DIR = DATA_DIR / "training_dir"
+TRAINING_DIR = DATA_DIR / "training_dir"
 NVD_ENRICHED = DATA_DIR / "nvd_data_with_predictions"
 PREPROCESS_FILEPATH = DATA_DIR / "preprocessed"
 
@@ -32,6 +31,7 @@ def parse_args():
             "train_cwe_model",
             "enrich_corpus",
             "preprocess",
+            "feature_generation"
         ],
     )
     parser.add_argument("--input", help="Input text for prediction mode")
@@ -40,32 +40,30 @@ def parse_args():
 
 def main():
     args = parse_args()
-    try:
-        if args.mode == "fetch_corpus":
-            download_and_process_cve_data(nvd_filepath=NVD_FILEPATH)
-        elif args.mode == "prepare_data":
-            extract_training_set(data_dir=DATA_DIR, nvd_filepath=NVD_FILEPATH)
-        elif args.mode == "train_cvss_model":
-            train_cvss_model()
-        elif args.mode == "train_cwe_model":
-            train_cwe_classifier()
-        elif args.mode == "enrich_corpus":
-            predict_cwe_and_cvss_for_nvd_data()
-        elif args.mode == "preprocess":
-            cve_data = process_cve_entries(nvd_filepath=NVD_ENRICHED)
-            df = pd.DataFrame(cve_data)
-            PREPROCESS_FILEPATH.mkdir(parents=True, exist_ok=True)
-            save_filepath = (
-                PREPROCESS_FILEPATH / "processed_nvd_data_with_predictions.csv"
-            )
-            df.to_csv(save_filepath, index=False)
-            print(f"Data has been processed and saved to '{save_filepath}'")
-        elif args.mode == "feature_generation":
-            feature_generation()
-        else:
-            raise ValueError(f"Invalid mode: {args.mode}")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
+    if args.mode == "fetch_corpus":
+        download_and_process_cve_data(nvd_filepath=NVD_FILEPATH)
+    elif args.mode == "prepare_data":
+        extract_training_set(data_dir=TRAINING_DIR, nvd_filepath=NVD_FILEPATH)
+    elif args.mode == "train_cvss_model":
+        train_cvss_model()
+    elif args.mode == "train_cwe_model":
+        train_cwe_classifier()
+    elif args.mode == "enrich_corpus":
+        predict_cwe_and_cvss_for_nvd_data()
+    elif args.mode == "preprocess":
+        cve_data = process_cve_entries(nvd_filepath=NVD_ENRICHED)
+        df = pd.DataFrame(cve_data)
+        PREPROCESS_FILEPATH.mkdir(parents=True, exist_ok=True)
+        save_filepath = (
+            PREPROCESS_FILEPATH / "processed_nvd_data_with_predictions.csv"
+        )
+        df.to_csv(save_filepath, index=False)
+        print(f"Data has been processed and saved to '{save_filepath}'")
+    elif args.mode == "feature_generation":
+        feature_generation()
+    else:
+        raise ValueError(f"Invalid mode: {args.mode}")
+
 
 
 if __name__ == "__main__":
